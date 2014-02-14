0d9d0f7246a5e1bc3a685e370003c5b1a63cb0ba
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-02-14 10:16:48 -0500

    Fix tests.

--- tests/ClientTests.fs
+++ tests/ClientTests.fs
@@ -32,20 +32,20 @@ module ClientTests =
 
         Test "Test0001" {
             let x = Tests.Test0001.x
-            x.a <- 12
-            x.a =? 12
+            x.a <- 12.
+            x.a =? 12.
             x.b =? "a"
             box x.c =? box 0
             x.d =? [||]
             x.e =? true
-            x.incr(1, "A") =? 2
-            x.withRest(1, true, "A", "B") =? "A,B,1,true"
+            x.incr(1., "A") =? 2.
+            x.withRest(1., true, "A", "B") =? "A,B,1,true"
         }
 
         Test "Test0002" {
             let w = Tests.window
-            (w.outerHeight > 0) =? true
+            (w.outerHeight > 0.) =? true
             w.location.href.Contains("://") =? true
-//            Tests.isNaN(1) =? false
-//            Tests.isNaN(As nan) =? true
+            Tests.isNaN(1.) =? false
+            Tests.isNaN(nan) =? true
         }
