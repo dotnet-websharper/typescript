5d9fc2ef8b8bcd6424f09518f860e1d3651cd92e
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-03-12 16:37:11 -0400

    Fix #28

--- scripts/Lib.d.ts
+++ scripts/Lib.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="../contrib/DefinitelyTyped-master/_infrastructure/tests/typescript/0.9.5/lib.d.ts" />
+/// <reference path="../contrib/DefinitelyTyped-master/_infrastructure/tests/typescript/0.9.7/lib.d.ts" />
