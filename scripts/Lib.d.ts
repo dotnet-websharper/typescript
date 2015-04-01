e3316a9da4f3c46b1268f794234d2e01f9aac378
Andr?s Jank? <andras.janko@gmail.com>
2015-04-01 14:27:46 +0200

    fix lib.d.ts path, add app.config to builder

--- scripts/Lib.d.ts
+++ scripts/Lib.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="../contrib/DefinitelyTyped-master/_infrastructure/tests/typescript/0.9.7/lib.d.ts" />
+/// <reference path="../contrib/DefinitelyTyped-0.9.5/_infrastructure/tests/typescript/0.9.5-beta/lib.d.ts" />
