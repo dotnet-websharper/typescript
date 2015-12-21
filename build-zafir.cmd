92d42d9be6b327828dc46e25f25c0faeaa7e0ef8
jankoa <andras.janko@gmail.com>
2015-12-21 10:19:11 +0100

    build with MSBuild 14

--- build-zafir.cmd
+++ build-zafir.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 setlocal
-set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\12.0\Bin\
+set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin\
 set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 MSBuild.exe msbuild/Main.Zafir.proj %*
