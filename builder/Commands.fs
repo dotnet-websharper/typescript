1f788701820e2f0a66dbf232c82dc7775a05ceba
loic <loic@denuziere.net>
2017-02-24 14:38:51 -0800

    Compute version in a script, for some reason in exe it can't find existing

--- builder/Commands.fs
+++ builder/Commands.fs
@@ -146,7 +146,15 @@ module internal Commands =
 
     let FsiExec script args =
         Command {
-            let fsi = PathUtility.GetFsiPath()
+            let fsiDir = Path.Combine(SolutionDirectory, "tools")
+            let fsi = Path.Combine(fsiDir, "fsi.exe")
+            let dir = Path.GetDirectoryName(PathUtility.GetFsiPath())
+            let copy x =
+                let o = Path.Combine(fsiDir, x)
+                if not (File.Exists(o)) then
+                    File.Copy(Path.Combine(dir, x), o)
+            copy "Fsi.exe"
+            copy "FSharp.Compiler.Interactive.Settings.dll"
 #if ZAFIR
             do! Execute fsi "--exec --define:ZAFIR %s %s" script args
 #else
