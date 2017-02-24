0a6db4e47e35e82a0619275c8711335d95cb44d6
loic <loic@denuziere.net>
2017-02-24 14:41:03 -0800

    Fix computeVersion.fsx

--- scripts/computeVersion.fsx
+++ scripts/computeVersion.fsx
@@ -2,10 +2,13 @@
 #r "../tools/packages/NuGet.Core/lib/net40-Client/NuGet.Core.dll"
 #r "../tools/packages/IntelliFactory.Build/lib/net40/IntelliFactory.Build.dll"
 
+open System.IO
+open IntelliFactory.Build
+
 let version =
-    IntelliFactory.Build.BuildTool()
+    BuildTool()
         .PackageId("Zafir.TypeScript")
         .VersionFrom("Zafir", "alpha")
-    |> IntelliFactory.Build.PackageVersion.Full.Find
+    |> PackageVersion.Full.Find
 
 File.WriteAllText(__SOURCE_DIRECTORY__ + "/../version.txt", version.ToString())
