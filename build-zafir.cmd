4230ed7fb5b67569b61f8452be08fa69c2ddd61c
Loic Denuziere <loic@denuziere.net>
2017-05-18 09:55:09 +0200

    Use fsi from FSharp.Compiler.Tools
    Avoids issues due to different compiler versions installed on the system.

--- build-zafir.cmd
+++ build-zafir.cmd
@@ -3,4 +3,6 @@ setlocal
 set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin\
 set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 tools\NuGet\NuGet.exe install IntelliFactory.Build -nocache -pre -excludeVersion -o tools/packages
+tools\NuGet\NuGet.exe install FSharp.Compiler.Tools -nocache -version 4.0.1.21 -excludeVersion -o tools/packages
+copy tools\fsi.exe.config tools\packages\FSharp.Compiler.Tools\tools\fsi.exe.config
 MSBuild.exe msbuild/Main.Zafir.proj %*
