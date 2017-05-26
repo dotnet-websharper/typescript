44abdaff93976a9825062723fc2f4845eaaf2e85
Andras Janko <andras.janko@gmail.com>
2017-05-26 13:21:20 +0200

    make -zafir build scripts default, add F#4.0 path

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 setlocal
+set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0
 set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin\
 set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 MSBuild.exe msbuild/Main.Zafir.proj %*
