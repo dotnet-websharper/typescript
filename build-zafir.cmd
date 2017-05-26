57259c71f605671f59121bc0e2bbd91cd0e88a10
Andras Janko <andras.janko@gmail.com>
2017-05-26 13:21:20 +0200

    make -zafir build scripts default, add F#4.0 path

--- build-zafir.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-setlocal
-set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin\
-set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-tools\NuGet\NuGet.exe install IntelliFactory.Build -nocache -pre -excludeVersion -o tools/packages
-MSBuild.exe msbuild/Main.Zafir.proj %*
