b6f8c749a35ee072ca344041afe3ec51c985efc0
Andras Janko <andras.janko@gmail.com>
2017-05-30 14:34:55 +0200

    use MsBuild 15.0

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 setlocal
-set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0
-set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin\
-set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%PATH%
+set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%PATH%
+set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%PATH%
 tools\NuGet\NuGet.exe install IntelliFactory.Build -nocache -pre -excludeVersion -o tools/packages
-MSBuild.exe msbuild/Main.Zafir.proj %*
+MSBuild.exe msbuild/Main.Zafir.proj /verbosity:minimal /p:VisualStudioVersion=15.0 /p:Arguments="%*" /fileLogger /flp:PerformanceSummary
