bf02585b0b3b1f658fc73117881c9eda03429b74
Andras Janko <andras.janko@gmail.com>
2017-05-30 16:36:20 +0200

    use 4.1 FCT

--- build.cmd
+++ build.cmd
@@ -4,6 +4,6 @@ set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0
 set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;%PATH%
 set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;%PATH%
 tools\NuGet\NuGet.exe install IntelliFactory.Build -nocache -pre -excludeVersion -o tools/packages
-tools\NuGet\NuGet.exe install FSharp.Compiler.Tools -nocache -version 4.0.1.21 -excludeVersion -o tools/packages
+tools\NuGet\NuGet.exe install FSharp.Compiler.Tools -nocache -version 4.1.17 -excludeVersion -o tools/packages
 copy tools\fsi.exe.config tools\packages\FSharp.Compiler.Tools\tools\fsi.exe.config
 MSBuild.exe msbuild/Main.Zafir.proj /verbosity:minimal /p:VisualStudioVersion=15.0 /p:Arguments="%*" /fileLogger /flp:PerformanceSummary
