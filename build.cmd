1df98724e4f58e311b4ce85c3a8f8239f19e3c27
Loic Denuziere <loic@denuziere.net>
2018-06-25 16:13:56 +0200

    Fix: use FSharp.Core 4.5 in Builder

--- build.cmd
+++ build.cmd
@@ -6,5 +6,6 @@ set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.
 tools\NuGet\NuGet.exe install FSharp.Core -nocache -version 4.1.18 -excludeVersion -o tools/packages
 tools\NuGet\NuGet.exe install IntelliFactory.Build -nocache -pre -excludeVersion -o tools/packages
 tools\NuGet\NuGet.exe install FSharp.Compiler.Tools -nocache -version 10.0.2 -excludeVersion -o tools/packages
+tools\NuGet\NuGet.exe install FSharp.Core -nocache -version 4.5.0 -o tools/packages
 copy tools\fsi.exe.config tools\packages\FSharp.Compiler.Tools\tools\fsi.exe.config
 MSBuild.exe msbuild/Main.Zafir.proj /verbosity:minimal /p:VisualStudioVersion=15.0 /p:Arguments="%*" /fileLogger /flp:PerformanceSummary
